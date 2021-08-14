8c24db37847f56c5ed7f2885e6d45f902b1cd7df
genturarif <gentur1608@gmail.com>
2021-08-14 15:08:18 +0700

    add local storage & fix bug

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 // Dark Mode
 let icon = document.getElementById("icon");
+
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
     icon.src = "img/dark theme icon/sun.png";
+    localStorage.setItem("theme", "dark");
   } else {
     icon.src = "img/dark theme icon/moon.png";
+    localStorage.removeItem("theme");
   }
 };
 
+if (localStorage.getItem("theme") == "dark") {
+  icon.onclick();
+}
+
 // Hamburger Menu
 let nav = document.querySelector("nav");
-nav.onclick = function () {
+
+if (document.body.classList.contains("dark-theme")) {
+  nav.classList.add("navbar-dark");
+} else {
+  nav.classList.remove("navbar-dark");
+}
+
+icon.addEventListener("click", () => {
   if (document.body.classList.contains("dark-theme")) {
     nav.classList.add("navbar-dark");
   } else {
     nav.classList.remove("navbar-dark");
   }
-};
+});
